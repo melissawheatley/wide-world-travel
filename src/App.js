3fd35fe4cd9ceb04f2770d30d19e7ae5334582e4
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-04-09 14:11:35 -0500

    moved footer code around to allow passing in props...

--- src/App.js
+++ src/App.js
@@ -7,11 +7,23 @@ import Footer from './Footer';
 
 class App extends Component {
   render() {
+  
     return (
       <div className="App">
         <Header Title="Wide World Travel, Inc" />
         <Main PrettyPhoto={PrettyPhoto} />
-        <Footer />
+        <footer>
+          <div>
+          <h3>Destinations</h3>
+            <ul>
+              <Footer Destination="China" />
+              <Footer Destination="Egypt" />
+              <Footer Destination="Portugal" />
+              <Footer Destination="Greece" />
+            </ul>
+          <p className = "Copyright">&copy; 2018 World Wide Travel</p>
+          </div>
+        </footer>
       </div>
     );
   }
--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,8 @@
 import React from 'react';
 
 function Footer(props){
-
     return (
-    <footer>
-        <div>
-        <h3>Destinations</h3>
-        <ul>
-            <li><a href="#">Iceland</a></li>
-            <li><a href="#">Democratic Republic of Congo</a></li>
-            <li><a href="#">New Zealand</a></li>
-            <li><a href="#">Greece</a></li>
-        </ul>
-        <p className = "Copyright">&copy; 2018 World Wide Travel</p>
-        </div>
-    </footer>
+        <li><a href="#">{props.Destination}</a></li>
     );
 }
 
