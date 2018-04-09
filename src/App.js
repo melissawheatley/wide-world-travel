43c7ff2545139507d8d9308843a3b9abf29470bf
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-04-09 13:12:52 -0500

    passing info into components with props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 import Main from './Main-body';
+import PrettyPhoto from './img/ICELAND.jpg';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Wide World Travel, Inc.</h1>
-        </header>
-        <Main />
+        <Header Title="Wide World Travel, Inc" />
+        <Main PrettyPhoto={PrettyPhoto} />
+        <Footer />
       </div>
     );
   }
--- src/Main-body.js
+++ src/Main-body.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import prettyPhoto from './img/ICELAND.jpg';
+
+
 
 function Main(props){
 
     return (
-    <main className="Pretty-Picture">
+    <div>
         {/* this is a comment. A comment cannot be the first line after a return */}
         {console.log('hello from Main-body.js')}
-        <img src={prettyPhoto} alt="Iceland with Northern Lights" />
-        <h1>World Wide Travel Main Body</h1>
-        <p>This is the main body text.</p>
-    </main>
+        <img className="Pretty-Picture" src={props.PrettyPhoto} alt="Iceland with Northern Lights" />
+        <main>
+            <h1>World Wide Travel Main Body</h1>
+            <p>This is the main body text.</p>
+        </main>
+    </div>
     );
 }
 
